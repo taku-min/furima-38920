d833ba06d5229a9e36cb0ac1ea2b0c45133008ba
taku-min <tw027444www422277@gmail.com>
2023-06-11 18:24:15 +0900

    order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,29 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
   def index
+    @item_order = ItemOrder.new
+  end
+
+  def create
+    @item_order = ItemOrder.new(order_params)
+    if @item_order.valid?
+      pay_item
+      @item_order.save
+      return redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number)
   end
 end

154555f33f3bf5676d29480b1e0e135cfddcc05f
taku-min <tw027444www422277@gmail.com>
2023-06-18 20:39:48 +0900

    新規登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,43 @@
 class ItemsController < ApplicationController
+  # before_action :authenticate_user!, except: [:index, :show]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+
   def index
+    #@items = Item.all.order(created_at: "DESC")
   end
+
+  # def show
+  # end
+
+  # # def new
+  # #   @item = Item.new
+  # # end
+
+  # def create
+  # end
+  
+   def edit
+   end
+
+   def update
+     if @item.update(item_params)
+       redirect_to item_path(@item.id)
+     else
+       render :edit
+     end
+   end
+
+  # def destroy
+  # end
+
+
+  #  private
+
+  #   def set_item
+  #   end
+
+  #  def item_params
+  #    params.require(:item).permit(:user, :description, :name, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  #  end 
 end

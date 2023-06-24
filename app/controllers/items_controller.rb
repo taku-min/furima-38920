2d67e6798b1f18ac8020612c39758ab657085cf4
taku-min <tw027444www422277@gmail.com>
2023-06-24 21:06:19 +0900

    ActiveStorageまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
 
   def index
     @items = Item.all.order(created_at: "DESC")
   end
 
-  # def show
-  # end
+  def show
+  end
 
   def new
     @item = Item.new
@@ -23,19 +23,19 @@ class ItemsController < ApplicationController
     end
   end
   
-  #  def edit
-  #  end
+   def edit
+   end
 
-  #  def update
-  #    if @item.update(item_params)
-  #      redirect_to item_path(@item.id)
-  #    else
-  #      render :edit
-  #    end
-  #  end
+   def update
+     if @item.update(item_params)
+       redirect_to item_path(@item.id)
+     else
+       render :edit
+     end
+   end
 
-  # def destroy
-  # end
+  def destroy
+  end
 
 
   private
@@ -44,6 +44,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:user, :description, :name, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :user, :description, :name, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cos
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end

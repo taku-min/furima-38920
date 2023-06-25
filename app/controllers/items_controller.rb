78e421b16b79355b4c47b500e494c7c6d14d9f54
taku-min <tw027444www422277@gmail.com>
2023-06-25 19:01:16 +0900

    テストコードまで

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,10 @@ class ItemsController < ApplicationController
   end
   
    def edit
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
    end
 
    def update
@@ -35,12 +39,19 @@ class ItemsController < ApplicationController
    end
 
   def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
 
   private
 
   def set_item
+    @item = Item.find(params[:id])
   end
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   belongs_to :category
   belongs_to :item_status
-  belongs_to :shipping_cos
+  belongs_to :shipping_cost
   belongs_to :prefecture
   belongs_to :shipping_date
 
@@ -14,6 +15,15 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
+  end
+
+  with_options numericality: {other_than: 0} do
     validates :category_id
     validates :item_status_id
     validates :shipping_cost_id

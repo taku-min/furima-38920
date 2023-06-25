0d18ffc42653774defd5f2c22368bb973b2d566b
taku-min <tw027444www422277@gmail.com>
2023-06-25 20:46:47 +0900

    java

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,13 +1,11 @@
-function item_price (){
-  const itemPrice = document.getElementById("item_price");
-  itemPrice.addEventListener("input", () => {
+window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     const ProfitDom = document.getElementById("profit");
 
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
-  })
-};
-
-window.addEventListener('load', item_price);
+    })
+  });

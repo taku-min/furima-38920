cbe9afae0a4423bcdfd98fd1b37275256d51da03
taku-min <tw027444www422277@gmail.com>
2023-07-02 15:22:45 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,14 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-  #before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
 
 
   def index
     @items = Item.all.order(created_at: "DESC")
   end
 
-  # def show
-  # end
+  def show
+  end
 
   def new
     @item = Item.new
@@ -23,12 +23,12 @@ class ItemsController < ApplicationController
     end
   end
   
-  #  def edit
-  #   if @item.user_id == current_user.id && @item.order.nil?
-  #   else
-  #     redirect_to root_path
-  #   end
-  #  end
+   def edit
+    if @item.user_id == current_user.id 
+    else
+      redirect_to root_path
+    end
+   end
 
   #  def update
   #    if @item.update(item_params)
@@ -50,9 +50,9 @@ class ItemsController < ApplicationController
 
   private
 
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :description, :name, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)

313c3dea042d053251a0b735d3a4b49c772f0ccf
taku-min <tw027444www422277@gmail.com>
2023-07-12 19:32:37 +0900

    商品購入機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,29 +1,45 @@
 class OrdersController < ApplicationController
-  # before_action :authenticate_user!
-  # before_action :set_item, only: [:index, :create]
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
 
-  # def index
-  #   @item_order = ItemOrder.new
-  # end
+  def index
+    if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
+      @order_form = OrderForm.new
+    else
+      redirect_to root_path
+    end
+  end
 
-  # def create
-  #   @item_order = ItemOrder.new(order_params)
-  #   if @item_order.valid?
-  #     pay_item
-  #     @item_order.save
-  #     return redirect_to root_path
-  #   else
-  #     render action: :index
-  #   end
-  # end
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
 
-  # private
+   private
 
-  # def set_item
-  #   @item = Item.find(params[:item_id])
-  # end
+  
+  def order_params
+    params.require(:order_form).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
 
-  # def order_params
-  #   params.require(:item_order).permit(:postal_code, :prefecture_id, :city_name, :block_name, :building_name, :phone_number)
-  # end
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+    
+  end
+
 end
